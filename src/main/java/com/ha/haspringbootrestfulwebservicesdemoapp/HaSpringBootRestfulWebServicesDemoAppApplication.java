c30283556b21977e60984018732b3c34f7a14669
hasiabewardana <hasi.prasanga@gmail.com>
2023-06-14 01:24:26 +0530

    Initial commit

---
+++ src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/HaSpringBootRestfulWebServicesDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.ha.haspringbootrestfulwebservicesdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaSpringBootRestfulWebServicesDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaSpringBootRestfulWebServicesDemoAppApplication.class, args);
+	}
+
+}
