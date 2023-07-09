10b09fc134a0c16a6252270977b10617875fb247
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-09 23:08:25 +0530

    Implemented i18n.

--- src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/helloworld/HelloWorldController.java
+++ src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/helloworld/HelloWorldController.java
@@ -1,15 +1,31 @@
 package com.ha.haspringbootrestfulwebservicesdemoapp.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("greeting", null, "Default Message", locale);
     }
 
     @GetMapping(path = "/hello-world-bean")
