8124616cbc67eb2b1406cf8747e69ab358ee0e6d
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-15 19:24:46 +0530

    Customized the field names and implemented the static filtering.

--- src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/domain/User.java
+++ src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/domain/User.java
@@ -1,16 +1,24 @@
 package com.ha.haspringbootrestfulwebservicesdemoapp.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+// Static filtering in class level
+// @JsonIgnoreProperties({"id", "birth_date"})
 public class User {
 
+    @JsonIgnore // Static filtering in property level
     private Integer id;
-    @Size(min = 2, message = "Name should have at least 2 letters.")
+    @Size(min = 2, message = "Name should have at least 2 letters.") // Validations
+    @JsonProperty("user_name") // Customize property names
     private String name;
-    @Past(message = "Birth date should be a past date.")
+    @Past(message = "Birth date should be a past date.") // Validations
+    @JsonProperty("birth_date") // Customize property names
+    @JsonIgnore // Static filtering in property level
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
@@ -45,10 +53,6 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", birthDate=" + birthDate +
-                '}';
+        return "User{" + "id=" + id + ", name='" + name + '\'' + ", birthDate=" + birthDate + '}';
     }
 }
