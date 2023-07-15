4600f945ee804d608b663e2a00e29061339e5051
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-15 19:12:19 +0530

    Implemented the HATEOAS concept in retrieveOneUser method.

--- src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/controller/UserController.java
+++ src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/controller/UserController.java
@@ -4,12 +4,17 @@ import com.ha.haspringbootrestfulwebservicesdemoapp.dao.UserDaoService;
 import com.ha.haspringbootrestfulwebservicesdemoapp.domain.User;
 import com.ha.haspringbootrestfulwebservicesdemoapp.exception.UserNotFoundException;
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -92,15 +97,23 @@ public class UserController {
     }
 
     // Get a specific user
+    // HATEOAS implementation
     @GetMapping("/users/{id}")
-    public User retrieveOneUser(@PathVariable int id) {
+    public EntityModel<User> retrieveOneUser(@PathVariable int id) {
         User user = userDaoService.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException("id: " + id);
         }
 
-        return user;
+        // Entity model
+        EntityModel entityModel = EntityModel.of(user);
+
+        // WebMvcLinkBuilder
+        WebMvcLinkBuilder webMvcLinkBuilder = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(webMvcLinkBuilder.withRel("all-users"));
+
+        return entityModel;
     }
 
     // Delete a specific user
