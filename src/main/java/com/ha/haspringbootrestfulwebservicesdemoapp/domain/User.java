6ab668ad34e609d397994aa005cfe6530105aa1e
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-16 14:09:51 +0530

    Connected to a H2 database.

--- src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/domain/User.java
+++ src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/domain/User.java
@@ -2,6 +2,9 @@ package com.ha.haspringbootrestfulwebservicesdemoapp.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,22 +12,29 @@ import java.time.LocalDate;
 
 // Static filtering in class level
 // @JsonIgnoreProperties({"id", "birth_date"})
+@Entity(name = "user_details")
 public class User {
 
-    @JsonIgnore // Static filtering in property level
+    // @JsonIgnore // Static filtering in property level
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 letters.") // Validations
     @JsonProperty("user_name") // Customize property names
     private String name;
     @Past(message = "Birth date should be a past date.") // Validations
     @JsonProperty("birth_date") // Customize property names
-    @JsonIgnore // Static filtering in property level
+    // @JsonIgnore // Static filtering in property level
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
+
     }
 
     public Integer getId() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(id, name, birth_date)
+values (1000, 'John Doe', current_date());
+
+insert into user_details(id, name, birth_date)
+values (1001, 'David Watson', current_date());
+
+insert into user_details(id, name, birth_date)
+values (1002, 'Kane Williams', current_date());
