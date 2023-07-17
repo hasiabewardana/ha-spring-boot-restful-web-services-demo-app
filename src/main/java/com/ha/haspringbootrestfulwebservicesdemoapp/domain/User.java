91436c65fc4c257018ec9c9117898e8a8090758e
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-17 17:20:14 +0530

    Created the Post entity and updated the H2 table with some values from data.sql

--- src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/domain/User.java
+++ src/main/java/com/ha/haspringbootrestfulwebservicesdemoapp/domain/User.java
@@ -1,13 +1,16 @@
 package com.ha.haspringbootrestfulwebservicesdemoapp.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
 // Static filtering in class level
 // @JsonIgnoreProperties({"id", "birth_date"})
@@ -18,13 +21,19 @@ public class User {
     @Id
     @GeneratedValue
     private Integer id;
+
     @Size(min = 2, message = "Name should have at least 2 letters.") // Validations
     @JsonProperty("user_name") // Customize property names
     private String name;
+
     @Past(message = "Birth date should be a past date.") // Validations
     @JsonProperty("birth_date") // Customize property names
     // @JsonIgnore // Static filtering in property level
     private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,12 @@ values (1001, 'David Watson', current_date());
 
 insert into user_details(id, name, birth_date)
 values (1002, 'Kane Williams', current_date());
+
+insert into post(id, description, user_id)
+values(1, 'I want to master Microservices.', 1000);
+
+insert into post(id, description, user_id)
+values(2, 'I want to learn AWS.', 1001);
+
+insert into post(id, description, user_id)
+values(3, 'I want to learn Java.', 1001);
